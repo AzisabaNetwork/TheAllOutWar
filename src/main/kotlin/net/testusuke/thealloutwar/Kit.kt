b27c5f3926557ef640d7387a7a43a73c40e44601
testusuke <silverbro007@icloud.com>
2022-05-03 11:39:51 +0900

    feat: created Kit

---
+++ src/main/kotlin/net/testusuke/thealloutwar/Kit.kt
@@ -0,0 +1,11 @@
+package net.testusuke.thealloutwar
+
+/**
+ * Created by testusuke on 2022/05/02
+ * @author testusuke
+ */
+data class Kit(
+    val id: Int,
+    val name: String,
+    val base64: String
+)
