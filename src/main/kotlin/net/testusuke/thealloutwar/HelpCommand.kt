9e5debd271d00f5494f1c1a23e8729de54f391d8
testusuke <silverbro007@icloud.com>
2022-05-03 11:40:14 +0900

    feat: add help massage

--- src/main/kotlin/net/testusuke/thealloutwar/HelpCommand.kt
+++ src/main/kotlin/net/testusuke/thealloutwar/HelpCommand.kt
@@ -10,28 +10,25 @@ object HelpCommand {
 
     fun help(player: Player) {
         val msg = """
-            §d§l================================
-            §e§l/war help -> ヘルプ表示
-            §a§lユーザーコマンド
-            §e§l/war play register [team] -> ゲーム参加登録
-            §e§l/war play unregister -> ゲーム参加取り消し
-            §c§lアドミンコマンド
-            §a§lGameコマンド
-            §e§l/war
-            §a§lMapコマンド
-            §e§l/war map create <name> -> 指定した名前のMapを作成
-            §e§l/war map delete <name> -> マップの削除
-            §e§l/war map rename <old> <new> -> マップのリネーム
-            §e§l/war map board <name> -> 指定したマップのダッシュボード
-            §a§lKitコマンド
-            §e§l/war kit create <name> -> Kitを作成(所持しているアイテムがKitとして使われます)
-            §e§l/war kit delete <name> -> Kitの削除
-            §e§l/war kit update <name> -> Kitの更新
-            §e§l/war
-            §e§l/war
-            §e§l/war
-            §e§l/war
-            §d§l================================
+            §d================================
+            §e/war help -> ヘルプ表示
+            §aユーザーコマンド
+            §e/war play register [team] -> ゲーム参加登録
+            §e/war play unregister -> ゲーム参加取り消し
+            §cアドミンコマンド
+            §aGameコマンド
+            §e/war
+            §aMapコマンド
+            §e/war map create <name> -> 指定した名前のMapを作成
+            §e/war map delete <name> -> マップの削除
+            §e/war map rename <old> <new> -> マップのリネーム
+            §e/war map board <name> -> 指定したマップのダッシュボード
+            §aKitコマンド
+            §e/war kit create <name> -> Kitを作成(所持しているアイテムがKitとして使われます)
+            §e/war kit delete <name> -> Kitの削除
+            §e/war kit update <name> -> Kitの更新
+            §e/war
+            §d================================
         """.trimIndent()
         player.sendMessage(msg)
     }
