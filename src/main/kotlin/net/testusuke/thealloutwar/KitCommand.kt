93a2a6496a00757addfccc2aa4d8ea6709a87ce1
testusuke <silverbro007@icloud.com>
2022-05-08 23:47:01 +0900

    change: add some functions

--- src/main/kotlin/net/testusuke/thealloutwar/KitCommand.kt
+++ src/main/kotlin/net/testusuke/thealloutwar/KitCommand.kt
@@ -1,6 +1,5 @@
 package net.testusuke.thealloutwar
 
-import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 /**
@@ -10,27 +9,114 @@ import org.bukkit.entity.Player
 object KitCommand {
 
     fun onCommand(player: Player, args: Array<out String>) {
-        if (args.isEmpty()) {
-            player.sendMessage("§c構文が不正です。ヘルプを参照してください")
+        //  Permission
+        if (!player.hasPermission(Permission.KIT_GENERAL)) {
+            player.sendMessage(Message.PERMISSION_ERROR)
             return
         }
 
+        if (args.isEmpty()) {
+            player.sendHelp()
+            return
+        }
+
+        //  kit name
+        val name = args[1]
+
         when (args[0]) {
             "list" -> {
-
+                var msg = "§e=================キット一覧================="
+                val kitList = KitManager.list()
+                kitList.forEach {
+                    msg += "\n${it.name}"
+                }
+                player.sendMessage(msg)
             }
 
             "create" -> {
+                if (name.isEmpty()) {
+                    player.sendMessage(Message.INCORRECT_COMMAND_USAGE)
+                    return
+                }
+                //  check if kit is already existed
+                if (KitManager.getId(name) != null) {
+                    player.sendMessage(Message.KIT_ALREADY_EXISTED)
+                    return
+                }
 
+                //  inventory
+                val inv = player.inventory
+
+                //  create
+                val id = KitManager.createKit(name, inv)
+                if (id == null) {
+                    player.sendMessage(Message.DATABASE_ERROR)
+                    return
+                }
+
+                player.sendMessage(
+                    Message.KIT_SUCCESS_CREATING.replace("#0", name)
+                )
             }
 
             "delete" -> {
+                if (name.isEmpty()) {
+                    player.sendMessage(Message.INCORRECT_COMMAND_USAGE)
+                    return
+                }
+                //  check if kit is existed
+                val id = KitManager.getId(name)
+                if (id == null) {
+                    player.sendMessage(Message.KIT_NOT_EXISTED)
+                    return
+                }
 
+                //  delete
+                val result = KitManager.deleteKit(id) ?: false
+                if (!result){
+                    player.sendMessage(Message.DATABASE_ERROR)
+                    return
+                }
+
+                player.sendMessage(Message.KIT_SUCCESS_DELETING)
             }
 
             "update" -> {
+                if (name.isEmpty()) {
+                    player.sendMessage(Message.INCORRECT_COMMAND_USAGE)
+                    return
+                }
+                //  check if kit is existed
+                val id = KitManager.getId(name)
+                if (id == null) {
+                    player.sendMessage(Message.KIT_NOT_EXISTED)
+                    return
+                }
 
+                val inv = player.inventory
+                //  update
+                if (!KitManager.updateKit(id, inv)) {
+                    player.sendMessage(Message.DATABASE_ERROR)
+                    return
+                }
+                player.sendMessage(Message.KIT_SUCCESS_UPDATING)
+            }
+
+            "show" -> {
+
+            }
+
+            else -> {
+                player.sendMessage(Message.INCORRECT_COMMAND_USAGE)
             }
         }
     }
+
+    private fun Player.sendHelp() {
+        val msg = """
+            
+        """.trimIndent()
+        this.sendMessage(msg)
+    }
+
 }
--- src/main/kotlin/net/testusuke/thealloutwar/Message.kt
+++ src/main/kotlin/net/testusuke/thealloutwar/Message.kt
@@ -10,5 +10,16 @@ object Message {
     const val PERMISSION_ERROR = "§c権限がありません。"
 
     //  incorrect usage
-    const val INCORRECT_COMMAND_USAGE = "§c構文が不正です。ヘルプを参照してください"
+    const val INCORRECT_COMMAND_USAGE = "§c構文が不正です。ヘルプを参照してください。"
+
+    //  something wrong while using a database
+    const val DATABASE_ERROR = "§cデータベース操作時に、なんらかの問題が発生しました。"
+
+    /////////////////////
+    //  Kit
+    const val KIT_ALREADY_EXISTED = "§cすでにKitが登録されています。"
+    const val KIT_NOT_EXISTED = "§cKitが登録されていません。"
+    const val KIT_SUCCESS_CREATING = "§aKitを登録しました。name: #0"
+    const val KIT_SUCCESS_DELETING = "§aKitを削除しました。"
+    const val KIT_SUCCESS_UPDATING = "§aKitを削除しました。"
 }
