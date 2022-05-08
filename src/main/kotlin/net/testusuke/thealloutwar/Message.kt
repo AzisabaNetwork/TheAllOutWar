19a0ea68d952888f4ec5469731740a81ce330f1c
testusuke <silverbro007@icloud.com>
2022-05-08 22:40:23 +0900

    feat: create message object to manage all messages

---
+++ src/main/kotlin/net/testusuke/thealloutwar/Message.kt
@@ -0,0 +1,14 @@
+package net.testusuke.thealloutwar
+
+/**
+ * Created by testusuke on 2022/05/08
+ * @author testusuke
+ */
+object Message {
+
+    //  permission error
+    const val PERMISSION_ERROR = "§c権限がありません。"
+
+    //  incorrect usage
+    const val INCORRECT_COMMAND_USAGE = "§c構文が不正です。ヘルプを参照してください"
+}
