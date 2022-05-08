2e4b03708ed61adb283922f0ea174fa537efb494
testusuke <silverbro007@icloud.com>
2022-05-08 22:40:39 +0900

    feat: create permission object

---
+++ src/main/kotlin/net/testusuke/thealloutwar/Permission.kt
@@ -0,0 +1,11 @@
+package net.testusuke.thealloutwar
+
+/**
+ * Created by testusuke on 2022/05/08
+ * @author testusuke
+ */
+object Permission {
+
+    const val KIT_GENERAL = "thealloutwar.kit"
+    const val MAP_GENERAL = "thealloutwar.map"
+}
